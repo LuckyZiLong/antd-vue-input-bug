001ae53df445dbbed8b3b29a12d86e2d89b5d2f3
lzl <3189872707@qq.com>
2024-12-20 12:30:30 +0800

    提交Bug重现文件

---
+++ antd/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Form style="width: 340px;">
+      <FormItem>
+        <Input placeholder="输入" allow-clear>
+          <template #prefix>
+            Hello
+          </template>
+        </Input>
+      </FormItem>
+    </Form>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { Form, FormItem, Input } from "ant-design-vue";
+</script>
